a7d0bcfa6c95d71b0b3056c2d8a12f25ea723f31
aaron shivers <aaorn@moonmail.org>
2019-06-20 14:17:22 -0500

    added List Model

--- app.py
+++ app.py
@@ -1,8 +1,37 @@
 # Import Dependencies
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
 
 # Intitialize Application
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Configure Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Initialize Database
+db = SQLAlchemy(app)
+
+# Initialize Marshmallow
+ma = Marshmallow(app)
+
+# Create List Model
+class List(db.Model):
+  id = db.Column(db.Integer, primary_key=True)
+  name = db.Column(db.String(20), unique=True)
+  qty = db.Column(db.Integer)
+  measurement = db.Column(db.String(20))
+
+  def __init__(self, name, qty, measurement):
+    self.name = name
+    self.qty = qty
+    self.measurement = measurement
+
+
+
 
 # GET /
 @app.route('/')

7cfa2c697801d7b76bb84896e965ff8d6bb739bf
aaronshivers <aaron@moonmail.org>
2019-06-22 07:40:46 -0500

    roll back

--- app.py
+++ app.py
@@ -2,12 +2,14 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+import os
 
 # Intitialize Application
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Configure Database
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Initialize Database
